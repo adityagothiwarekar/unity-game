fe146ac17723032bfd30996fb4175c5ffc20d555
adityagothiwarekar <109978509+adityagothiwarekar@users.noreply.github.com>
2022-12-09 11:39:15 +0530

    Add files via upload

--- Obstacle.cs
+++ Obstacle.cs
@@ -1,53 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Obstacle : MonoBehaviour
 {
-    bool gameover = false;
+   public bool gameover;
+   public GameObject GameOverPanel;
+   public bool game=true;
     Rigidbody2D rb;
    public float rotationSpeed;
-   int score=0;
+   public TextMeshProUGUI textcoins;
+   public float score=0;
+   public float PointIncresedPerSecond = 1f;
+   [SerializeField] private AudioSource audiosource;
+   [SerializeField] private AudioSource gamemusic;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        
+       
        /* rb = GetComponent<Rigidbody2D>();
         rb.tag = "Player";
         Destroy(gameObject);
         */
+
     
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
     private void FixedUpdate()
     {
+        calculatescore();
+        transform.Rotate(0,0,rotationSpeed);
+       
+        
+      
+       
+       
+         }
+         
+        /*
         //to rotate the obstacle
 
         transform.Rotate(0,0,rotationSpeed);
-    }
+        if(gameover)
+        {
+           
+        textcoins.text = "Score :"+((int)score).ToString();
+        score += PointIncresedPerSecond * Time.fixedDeltaTime;
+        }else
+        {
+                                                                                                                                                                                                                           
+                textcoins.text="Score :"+((int)score).ToString();
+        }
+        */
+    
+    
+     private void OnTriggerEnter2D(Collider2D collision) {
+
+            if(collision.gameObject.CompareTag("Ground"))
+            {
+                   
+                gameover=false;
+                gamemusic.Play();
+              
+            }
+             if(collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("inside player");
+                gameover=true;
+                gamemusic.Pause();
+                Debug.Log("paused music");
+                audiosource.Play();
+                Debug.Log("Changed Gameover");
+                Destroy(collision.gameObject);
+                GameObject.FindGameObjectWithTag("ObstacleSpawner").GetComponent<Obstaclespawner>().StopSpawning();
+                GameOverPanel.SetActive(true);
+                score=0;
+                textcoins.text = "Score :"+((int)score).ToString();
+        }
+            }
+            
+     
+
+        public void calculatescore()
+        {
+            if(!gameover)
+        {
+          score=score/1000+1;
+         textcoins.text = "Score :"+((int)score).ToString();
+        }
+        }
+       
+}
+        
+    
     //to destroy the object once it touches the ground
-    public void OnCollisionEnter2D(Collision2D col) {
+   /* public void OnCollisionEnter2D(Collision2D col) {
 
         if(col.gameObject.tag =="Player")
         {
+        gameover=false;
         Debug.Log("collision occurs");
         Destroy(col.gameObject);
         GameObject.FindGameObjectWithTag("ObstacleSpawner").GetComponent<Obstaclespawner>().StopSpawning();
-        gameover=true;
-       
-    
-          }
-          else{
+        print(gameover);
+
+        }
+        else{
+
             
-            score++;
-            print(score);
-          }
+         }
         
         /*if(col.gameObject.tag =="Player")
         {
@@ -62,15 +135,31 @@ public class Obstacle : MonoBehaviour
         }
         */
         
-    }
 
-   /* public void IncrementScore()
-    {
-        if(!gameover)
-        {
-            score++;
-           Debug.Log("score");
-        }
-    }  
-    */ 
-}
+        
+    
+        
+        /*public void addscore(int newscore)
+         {
+            score=score+newscore;
+         }
+         public void updatescore()
+         {
+            textcoins.text = "Score :"+((int)score).ToString();
+
+         }
+         */
+         
+
+
+
+
+    
+
+        
+     
+
+     
+    
+
+
